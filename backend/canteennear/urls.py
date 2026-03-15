b0869c047371a8e2fad84f1d9b615b181f67f125
Arseniy Shugurov <shugurovarsenii@gmail.com>
2026-03-15 23:20:12 +0300

    Добавлен url, для переадресации на страницу canteen_detail

--- backend/canteennear/urls.py
+++ backend/canteennear/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path('admin/', admin.site.urls),
+    path("canteen/<int:id>/", views.canteen_detail, name="canteen_detail"),
 ]
