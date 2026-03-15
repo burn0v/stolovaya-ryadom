7775f8dfa2c1e204b701133634fbc59f8b906d5c
Arseniy Shugurov <shugurovarsenii@gmail.com>
2026-03-15 23:10:43 +0300

    Добавлено время работы и меню в models.py

--- backend/canteennear/models.py
+++ backend/canteennear/models.py
@@ -6,5 +6,8 @@ class Canteen(models.Model):
     city = models.CharField(max_length=100)
     rating = models.FloatField(default=0)
 
+    working_hours = models.CharField(max_length=200)
+    menu = models.TextField()
+
     def __str__(self):
         return self.name
